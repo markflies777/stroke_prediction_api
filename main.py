f59119363d48734947c12208fccab516cf2e1e7f
markflies777 <78021682+markflies777@users.noreply.github.com>
2022-01-07 19:10:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from flask import Flask, request
+from flask_cors import CORS, cross_origin
 from serve import prediction
 import pandas as pd
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 stroke_predict = prediction()
 
 
 @app.route('/predict', methods=['POST'])
+@cross_origin()
 def predict():
     input_data = request.json
     input_df = pd.DataFrame.from_dict([input_data])
